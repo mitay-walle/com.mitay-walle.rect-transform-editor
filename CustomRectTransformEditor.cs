46058ec889ff6cb5a0bba5aee4554e8c36c1110b
Dmitry <mitay-walle91@mail.ru>
2024-04-22 06:42:46 +0300

    fix OnSceneGUI called multiply times

--- CustomRectTransformEditor.cs
+++ CustomRectTransformEditor.cs
@@ -7,278 +7,294 @@ using Object = UnityEngine.Object;
 
 namespace Plugins.UI.Editor
 {
-    [CustomEditor(typeof(RectTransform), true), CanEditMultipleObjects]
-    public class CustomRectTransformEditor : UnityEditor.Editor
-    {
-        private static PropertyInfo _rectDrivenObject = typeof(RectTransform).GetProperty("drivenByObject"
-            , BindingFlags.NonPublic | BindingFlags.Instance);
-        private UnityEditor.Editor editorInstance;
-        private static Type nativeEditor;
-        private MethodInfo onSceneGui;
+	[CustomEditor(typeof(RectTransform), true), CanEditMultipleObjects]
+	public class CustomRectTransformEditor : UnityEditor.Editor
+	{
+		private static PropertyInfo _rectDrivenObject = typeof(RectTransform).GetProperty("drivenByObject"
+		                                                                                  , BindingFlags.Public | BindingFlags.Instance);
+		private UnityEditor.Editor editorInstance;
+		private static Type nativeEditor;
+		private MethodInfo onSceneGui;
 
-        private Rect _rect = new Rect(45, 65, 45, 18);
-        private Rect _rect2 = new Rect(70, 25, 20, 18);
-        private Rect _rect3 = new Rect(70, 45, 20, 18);
-        private Rect _rect4 = new Rect(0, 65, 45, 18);
+		private Rect _rect = new Rect(45, 65, 45, 18);
+		private Rect _rect2 = new Rect(70, 25, 20, 18);
+		private Rect _rect3 = new Rect(70, 45, 20, 18);
+		private Rect _rect4 = new Rect(0, 65, 45, 18);
+		bool _isOnSceneGUICalledThisFrame;
 
-        private void OnEnable()
-        {
-            if (targets.Length == 0 || targets[0] == null) return;
+		private void OnEnable()
+		{
+			if (targets.Length == 0 || targets[0] == null) return;
 
-            Initialize();
-            if (nativeEditor == null) return;
-            if (editorInstance == null) return;
+			Initialize();
+			if (nativeEditor == null) return;
+			if (editorInstance == null) return;
 
-            try
-            {
-                if (editorInstance.targets.Length > 0 && editorInstance.targets[0] != null)
-                {
-                    nativeEditor.GetMethod("OnEnable",
-                            BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic)
-                        ?.Invoke(editorInstance, null);
-                }
-            }
-            catch
-            {
-            }
+			try
+			{
+				if (editorInstance.targets.Length > 0 && editorInstance.targets[0] != null)
+				{
+					nativeEditor.GetMethod("OnEnable",
+					                       BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic)
+						?.Invoke(editorInstance, null);
+				}
+			}
+			catch
+			{
+			}
 
-            onSceneGui = nativeEditor.GetMethod("OnSceneGUI",
-                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-        }
+			onSceneGui = nativeEditor.GetMethod("OnSceneGUI",
+			                                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			SceneView.duringSceneGui -= OnSceneGUIEvent;
+			SceneView.duringSceneGui += OnSceneGUIEvent;
+		}
 
-        private void Initialize()
-        {
-            if (nativeEditor == null)
-            {
-                nativeEditor = Assembly.GetAssembly(typeof(UnityEditor.Editor))
-                    .GetType("UnityEditor.RectTransformEditor");
-            }
+		private void Initialize()
+		{
+			if (nativeEditor == null)
+			{
+				nativeEditor = Assembly.GetAssembly(typeof(UnityEditor.Editor))
+					.GetType("UnityEditor.RectTransformEditor");
+			}
 
-            if (editorInstance)
-            {
-                CreateCachedEditor(targets, nativeEditor, ref editorInstance);
-            }
-            else
-            {
-                editorInstance = CreateEditor(targets, nativeEditor);
-            }
-        }
+			if (editorInstance)
+			{
+				CreateCachedEditor(targets, nativeEditor, ref editorInstance);
+			}
+			else
+			{
+				editorInstance = CreateEditor(targets, nativeEditor);
+			}
+		}
 
-        public override void OnInspectorGUI()
-        {
-            editorInstance.OnInspectorGUI();
+		public override void OnInspectorGUI()
+		{
+			editorInstance.OnInspectorGUI();
 
-            bool needMoveY = NeedMoveY();
+			bool needMoveY = NeedMoveY();
 
-            Rect rect = _rect;
-            rect.y += needMoveY ? 20 : 0;
+			Rect rect = _rect;
+			rect.y += needMoveY ? 20 : 0;
 
-            // Code here
-            if (GUI.Button(rect, "Snap"))
-            {
-                foreach (Object targ in targets)
-                {
-                    if (targ is RectTransform rectTr)
-                    {
-                        Undo.RecordObject(targ, "Snap to parent");
-                        SnapToParent(rectTr);
-                        EditorUtility.SetDirty(targ);
-                    }
-                }
-            }
+			// Code here
+			if (GUI.Button(rect, "Snap"))
+			{
+				foreach (Object targ in targets)
+				{
+					if (targ is RectTransform rectTr)
+					{
+						Undo.RecordObject(targ, "Snap to parent");
+						SnapToParent(rectTr);
+						EditorUtility.SetDirty(targ);
+					}
+				}
+			}
 
-            rect = _rect4;
-            rect.y += needMoveY ? 20 : 0;
+			rect = _rect4;
+			rect.y += needMoveY ? 20 : 0;
 
-            if (GUI.Button(rect, "New"))
-            {
-                foreach (Object targ in targets)
-                {
-                    if (targ is RectTransform rectTr)
-                    {
-                        CreateEmptyParentRect(rectTr);
-                    }
-                }
-            }
+			if (GUI.Button(rect, "New"))
+			{
+				foreach (Object targ in targets)
+				{
+					if (targ is RectTransform rectTr)
+					{
+						CreateEmptyParentRect(rectTr);
+					}
+				}
+			}
 
-            if (targets.Length > 1)
-            {
-                GUI.enabled = false;
-            }
+			if (targets.Length > 1)
+			{
+				GUI.enabled = false;
+			}
 
-            rect = _rect2;
-            rect.y += needMoveY ? 20 : 0;
+			rect = _rect2;
+			rect.y += needMoveY ? 20 : 0;
 
-            if (GUI.Button(rect, "C"))
-            {
-                ComponentUtility.CopyComponent(target as RectTransform);
-            }
+			if (GUI.Button(rect, "C"))
+			{
+				ComponentUtility.CopyComponent(target as RectTransform);
+			}
 
-            GUI.enabled = true;
+			GUI.enabled = true;
 
-            rect = _rect3;
-            rect.y += needMoveY ? 20 : 0;
+			rect = _rect3;
+			rect.y += needMoveY ? 20 : 0;
 
-            if (GUI.Button(rect, "P"))
-            {
-                foreach (Object targ in targets)
-                {
-                    Undo.RecordObject(targ, "Paste");
-                    ComponentUtility.PasteComponentValues(target as RectTransform);
-                    EditorUtility.SetDirty(targ);
-                }
-            }
-        }
+			if (GUI.Button(rect, "P"))
+			{
+				foreach (Object targ in targets)
+				{
+					Undo.RecordObject(targ, "Paste");
+					ComponentUtility.PasteComponentValues(target as RectTransform);
+					EditorUtility.SetDirty(targ);
+				}
+			}
+		}
 
-        private void OnSceneGUI()
-        {
-            onSceneGui.Invoke(editorInstance, null);
-        }
+		private void OnSceneGUIEvent(SceneView sceneView)
+		{
+			_isOnSceneGUICalledThisFrame = false;
+		}
 
-        private void OnDisable()
-        {
-            if (editorInstance) DestroyImmediate(editorInstance);
-        }
+		private void OnSceneGUI()
+		{
+			if (_isOnSceneGUICalledThisFrame) return;
 
-        private static void SnapToParent(RectTransform rect)
-        {
-            rect.pivot = new Vector2(.5f, .5f);
-            rect.localScale = Vector3.one;
-            rect.localPosition = Vector3.zero;
-            rect.localRotation = Quaternion.identity;
-            rect.anchoredPosition = Vector2.zero;
-            rect.anchorMin = Vector2.zero;
-            rect.anchorMax = Vector2.one;
-            rect.sizeDelta = Vector2.zero;
-        }
+			_isOnSceneGUICalledThisFrame = true;
 
-        public static void CreateEmptyParentRect(RectTransform rect)
-        {
-            GameObject go = new GameObject("Create Empty Parent");
+			if (!(bool)this.target)
+				return;
 
-            ComponentUtility.CopyComponent(rect);
-            ComponentUtility.PasteComponentAsNew(go);
+			onSceneGui.Invoke(editorInstance, null);
+		}
 
-            Undo.RecordObject(rect, "Create Empty, Reparent");
+		private void OnDisable()
+		{
+			SceneView.duringSceneGui -= OnSceneGUIEvent;
+			if (editorInstance) DestroyImmediate(editorInstance);
+		}
 
-            Undo.RegisterCreatedObjectUndo(go, "Create Empty Parent");
+		private static void SnapToParent(RectTransform rect)
+		{
+			rect.pivot = new Vector2(.5f, .5f);
+			rect.localScale = Vector3.one;
+			rect.localPosition = Vector3.zero;
+			rect.localRotation = Quaternion.identity;
+			rect.anchoredPosition = Vector2.zero;
+			rect.anchorMin = Vector2.zero;
+			rect.anchorMax = Vector2.one;
+			rect.sizeDelta = Vector2.zero;
+		}
 
-            RectTransform rect2 = go.transform as RectTransform;
+		public static void CreateEmptyParentRect(RectTransform rect)
+		{
+			GameObject go = new GameObject("Create Empty Parent");
 
-            PlaceSameAs(go.transform, rect, true, true, true);
-            ComponentUtility.PasteComponentValues(rect2);
-            Undo.SetTransformParent(rect, go.transform, "Create Empty, Reparent");
-            SnapToParent(rect);
+			ComponentUtility.CopyComponent(rect);
+			ComponentUtility.PasteComponentAsNew(go);
 
-            EditorUtility.SetDirty(rect);
+			Undo.RecordObject(rect, "Create Empty, Reparent");
 
-            Selection.activeGameObject = go;
-            EditorGUIUtility.PingObject(go);
-        }
+			Undo.RegisterCreatedObjectUndo(go, "Create Empty Parent");
 
-        private static void PlaceSameAs(Transform target, Transform source, bool copyName = false, bool undo = false,
-            bool setDirty = false)
-        {
-            if (!source) return;
+			RectTransform rect2 = go.transform as RectTransform;
 
-            if (undo && Application.isPlaying)
-            {
-                Undo.RecordObject(target, "PlaceSameAs");
-                if (copyName) Undo.RecordObject(target.gameObject, "PlaceSameAs");
-            }
+			PlaceSameAs(go.transform, rect, true, true, true);
+			ComponentUtility.PasteComponentValues(rect2);
+			Undo.SetTransformParent(rect, go.transform, "Create Empty, Reparent");
+			SnapToParent(rect);
 
-            VectorArray matrix = new VectorArray(source, false);
-            matrix.Apply(target);
-            if (undo)
-            {
-                Undo.SetTransformParent(target, source.parent, "PlaceSameAs");
-            }
-            else
-            {
-                target.SetParent(source.parent);
-            }
+			EditorUtility.SetDirty(rect);
 
-            target.SetSiblingIndex(source.GetSiblingIndex());
+			Selection.activeGameObject = go;
+			EditorGUIUtility.PingObject(go);
+		}
 
-            if (copyName) target.name = source.name;
+		private static void PlaceSameAs(Transform target, Transform source, bool copyName = false, bool undo = false,
+		                                bool setDirty = false)
+		{
+			if (!source) return;
 
-            if (setDirty && Application.isPlaying)
-            {
-                EditorUtility.SetDirty(target);
-                if (copyName) EditorUtility.SetDirty(target.gameObject);
-            }
-        }
+			if (undo && Application.isPlaying)
+			{
+				Undo.RecordObject(target, "PlaceSameAs");
+				if (copyName) Undo.RecordObject(target.gameObject, "PlaceSameAs");
+			}
 
-        // ReSharper disable Unity.PerformanceAnalysis
-        private bool NeedMoveY()
-        {
-            bool value = false;
-            foreach (RectTransform target in targets)
-            {
-                if (NeedMoveY(target))
-                {
-                    value = true;
-                    break;
-                }
-            }
+			VectorArray matrix = new VectorArray(source, false);
+			matrix.Apply(target);
+			if (undo)
+			{
+				Undo.SetTransformParent(target, source.parent, "PlaceSameAs");
+			}
+			else
+			{
+				target.SetParent(source.parent);
+			}
 
-            return value;
-        }
+			target.SetSiblingIndex(source.GetSiblingIndex());
 
-        private bool NeedMoveY(RectTransform target) => target.drivenByObject != null;
-    }
+			if (copyName) target.name = source.name;
 
-    [Serializable]
-    public struct VectorArray
-    {
-        [SerializeField] public bool local;
-        [SerializeField] public Vector3 Pos;
-        [SerializeField] public Quaternion Rot;
-        [SerializeField] public Vector3 Scale;
+			if (setDirty && Application.isPlaying)
+			{
+				EditorUtility.SetDirty(target);
+				if (copyName) EditorUtility.SetDirty(target.gameObject);
+			}
+		}
 
-        public VectorArray(Transform tr, bool local)
-        {
-            this.local = local;
-            if (!tr)
-            {
-                Pos = default;
-                Rot = default;
-                Scale = default;
-            }
-            else
-            {
-                Scale = local ? tr.localScale : tr.lossyScale;
+		// ReSharper disable Unity.PerformanceAnalysis
+		private bool NeedMoveY()
+		{
+			bool value = false;
+			foreach (RectTransform target in targets)
+			{
+				if (NeedMoveY(target))
+				{
+					value = true;
+					break;
+				}
+			}
 
-                if (local)
-                {
-                    Pos = tr.localPosition;
-                    Rot = tr.localRotation;
-                }
-                else
-                {
-                    Pos = tr.position;
-                    Rot = tr.rotation;
-                }
-            }
-        }
+			return value;
+		}
 
-        public void Apply(Transform tr)
-        {
-            if (local)
-            {
-                tr.localPosition = Pos;
-                tr.localRotation = Rot;
-            }
-            else
-            {
-                tr.position = Pos;
-                tr.rotation = Rot;
-            }
+		private bool NeedMoveY(RectTransform target) => _rectDrivenObject.GetValue(target) != null;
+	}
 
-            tr.localScale = Scale;
+	[Serializable]
+	public struct VectorArray
+	{
+		[SerializeField] public bool local;
+		[SerializeField] public Vector3 Pos;
+		[SerializeField] public Quaternion Rot;
+		[SerializeField] public Vector3 Scale;
+
+		public VectorArray(Transform tr, bool local)
+		{
+			this.local = local;
+			if (!tr)
+			{
+				Pos = default;
+				Rot = default;
+				Scale = default;
+			}
+			else
+			{
+				Scale = local ? tr.localScale : tr.lossyScale;
+
+				if (local)
+				{
+					Pos = tr.localPosition;
+					Rot = tr.localRotation;
+				}
+				else
+				{
+					Pos = tr.position;
+					Rot = tr.rotation;
+				}
+			}
+		}
+
+		public void Apply(Transform tr)
+		{
+			if (local)
+			{
+				tr.localPosition = Pos;
+				tr.localRotation = Rot;
+			}
+			else
+			{
+				tr.position = Pos;
+				tr.rotation = Rot;
+			}
+
+			tr.localScale = Scale;
 #if UNITY_EDITOR
-            if (!Application.isPlaying) EditorUtility.SetDirty(tr);
+			if (!Application.isPlaying) EditorUtility.SetDirty(tr);
 #endif
-        }
-    }
+		}
+	}
 }
